6be483e8e52688a189c590e2206aa6a53e849584
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-18 13:19:46 +0530

    Initial Commit

---
+++ CupCake App iOS/CupCake_App_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCake_App_iOSApp.swift
+//  CupCake App iOS
+//
+//  Created by Shubham on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CupCake_App_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
