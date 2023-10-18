e53e86ea857ac7fd560bab87a59904cd67ae1517
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-18 21:13:57 +0530

    cupcake completed

--- CupCake App iOS/View/CheckoutView.swift
+++ CupCake App iOS/View/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     
     var body: some View {
         ScrollView {
@@ -25,7 +27,9 @@ struct CheckoutView: View {
                 Text("Your total cost is \(order.cost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
                 
                 Button("Place order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
                 .buttonStyle(.borderedProminent)
@@ -34,6 +38,34 @@ struct CheckoutView: View {
         
         .navigationTitle("Checkout")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") {}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    // MARK: Methods
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data,_) = try await URLSession.shared.upload(for: request, from: encoded)
+            // handle the result
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way"
+            showingConfirmation = true
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
--- CupCake App iOS/View/ContentView.swift
+++ CupCake App iOS/View/ContentView.swift
@@ -50,7 +50,7 @@ struct ContentView: View {
                 }
             }
             
-            .navigationTitle("Icecream Ordering")
+            .navigationTitle("Cupcake Ordering")
         }
     }
     
